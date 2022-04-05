ea84517af251f79c86772ce98b84a96922537580
[UPFT]KazunoriAoki <kazunori.aoki@up-frontier.jp>
2022-04-05 09:36:35 +0900

    add

---
+++ PaddingManager/Sources/PaddingManager/View+.swift
@@ -0,0 +1,17 @@
+//
+//  View+.swift
+//  
+//
+//  Created by kazunori.aoki on 2022/04/05.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+extension View {
+    func padding(vertical: CGFloat, horizontal: CGFloat) -> some View {
+        return self
+            .padding(.vertical, vertical)
+            .padding(.horizontal, horizontal)
+    }
+}
---
+++ PaddingManager/Tests/PaddingManagerTests/PaddingManagerTests.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import PaddingManager
+
+final class PaddingManagerTests: XCTestCase {
+
+}
